c56b32afc5659304a91695c631e2e991d1f1efba
Marco Vazquez <marcovazquez77@hotmail.com>
2018-06-29 22:49:33 -0500

    DEKHash function added

--- src/main/kotlin/MyApp.kt
+++ src/main/kotlin/MyApp.kt
@@ -1,13 +1,14 @@
 import files.FileHandler
+import hash.HashAlgoritm
+import tornadofx.App
 import views.Interface
-import models.Register
-import tornadofx.*
 
 class MyApp: App(Interface::class)
 fun main(args: Array<String>) {
     //Application.launch(MyApp::class.java,*args)
     val fileHandler = FileHandler()
-    fileHandler.write(Register("x", "entero", 4, "*", "pr",0))
-    fileHandler.write(Register("y", "entero", 4, "*", "pr",1))
-    fileHandler.read()
+    //fileHandler.write(Register("x", "entero", 4, "*", "pr",0))
+    //fileHandler.write(Register("y", "entero", 4, "*", "pr",1))
+    //fileHandler.read()
+    println(HashAlgoritm.getHash("int"))
 }
---
+++ src/main/kotlin/hash/HashAlgoritm.kt
@@ -0,0 +1,13 @@
+package hash
+
+object HashAlgoritm {
+    @JvmStatic
+    fun getHash(str: String): Long {
+        var hash = str.length.toLong()
+        for (i in 0 until str.length) {
+            hash = hash shl 5 xor (hash shr 27) xor str[i].toLong()
+        }
+        hash %= 353
+        return hash
+    }
+}

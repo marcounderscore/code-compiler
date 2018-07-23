fa959ac2206b5a94a16a145c16c0fef49a4f6322
Marco Vazquez <marcovazquez77@hotmail.com>
2018-07-23 12:40:28 -0500

    Minor tests done

--- src/main/kotlin/Test.java
+++ src/main/kotlin/Test.java
@@ -1,10 +1,5 @@
 import java.util.ArrayList;
 
 public class Test {
-    void test(){
-        ArrayList<ArrayList<String>> listOfLists = new ArrayList<ArrayList<String>>();
-        for(int i = 0; i < 10; i++)  {
-            listOfLists.add(new ArrayList<String>());
-        }
-    }
+    void test(){ }
 }
